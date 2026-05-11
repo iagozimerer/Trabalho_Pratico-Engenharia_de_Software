5f2a1cf2cfb527e8b2dd8cc93bbda1fcb006e070
Iago Zimerer <86073147+iagozimerer@users.noreply.github.com>
2026-05-11 20:52:21 -0300

    Create constants.js

---
+++ js/config/constants.js
@@ -0,0 +1,21 @@
+export const GAME_CONFIG = {
+    WORD_LENGTH: 5,
+    MAX_ATTEMPTS: 6,
+
+    POINTS: {
+        CORRECT_LETTER: 10,
+        PRESENT_LETTER: 5
+    },
+
+    TILE_STATUS: {
+        CORRECT: "correct",
+        PRESENT: "present",
+        ABSENT: "absent"
+    }
+};
+
+export const COLORS = {
+    correct: "#538d4e",
+    present: "#b59f3b",
+    absent: "#3a3a3c"
+};
